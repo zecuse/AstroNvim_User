35c0f33aa27ac918896f87488cb2040b1f13e10a
Cory Williamson <zekos@ymail.com>
2023-08-22 00:16:05 -0400

    Updated user mappings
    User mappings resembles default mappings.

--- mappings.lua
+++ mappings.lua
@@ -1,40 +1,72 @@
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
-return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
+local utils = require "astronvim.utils"
+local status = require "astronvim.utils.status"
+local buffer = require "astronvim.utils.buffer"
 
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+local get_icon = utils.get_icon
+local is_available = utils.is_available
+local maps = utils.empty_map_table()
 
-    -- mappings seen under group name "Buffer"
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+-- Buffers management
+-- tables with the `name` key will be registered with which-key if it's installed
+-- this is useful for naming menus
+maps.n["<leader>b"] = { name = "Buffers" }
+maps.n["<leader>bD"] = {
+  function()
+    status.heirline.buffer_picker(
+      function(bufnr) buffer.close(bufnr) end
+    )
+  end,
+  desc = "Pick to close",
 }
+-- navigate buffer tabs with `H` and `L`
+-- maps.n["L"] = {
+--   function() buffer.nav(vim.v.count > 0 and vim.v.count or 1) end,
+--   desc = "Next buffer",
+-- },
+-- maps.n["H"] = {
+--   function() buffer.nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+--   desc = "Previous buffer",
+-- },
+
+-- Session management
+maps.n["<leader>s"] = { desc = get_icon("Session", 1, true) .. "Session" }
+maps.n["<leader>S"] = nil
+if is_available "neovim-session-manager" then
+  maps.n["<leader>sl"] = { "<cmd>SessionManager! load_last_session<cr>", desc = "Load last session" }
+  maps.n["<leader>ss"] = { "<cmd>SessionManager! save_current_session<cr>", desc = "Save this session" }
+  maps.n["<leader>sd"] = { "<cmd>SessionManager! delete_session<cr>", desc = "Delete session" }
+  maps.n["<leader>sf"] = { "<cmd>SessionManager! load_session<cr>", desc = "Search sessions" }
+  maps.n["<leader>s."] =
+    { "<cmd>SessionManager! load_current_dir_session<cr>", desc = "Load current directory session" }
+  maps.n["<leader>Sl"] = nil
+  maps.n["<leader>Ss"] = nil
+  maps.n["<leader>Sd"] = nil
+  maps.n["<leader>Sf"] = nil
+  maps.n["<leader>S."] = nil
+end
+if is_available "resession.nvim" then
+  local resession = require "resession"
+  maps.n["<leader>sl"] = { function() resession.load "Last Session" end, desc = "Load last session" }
+  maps.n["<leader>ss"] = { function() resession.save() end, desc = "Save this session" }
+  maps.n["<leader>st"] = { function() resession.save_tab() end, desc = "Save this tab's session" }
+  maps.n["<leader>sd"] = { function() resession.delete() end, desc = "Delete a session" }
+  maps.n["<leader>sf"] = { function() resession.load() end, desc = "Load a session" }
+  maps.n["<leader>s."] = {
+    function() resession.load(vim.fn.getcwd(), { dir = "dirsession" }) end,
+    desc = "Load current directory session",
+  }
+  maps.n["<leader>Sl"] = nil
+  maps.n["<leader>Ss"] = nil
+  maps.n["<leader>St"] = nil
+  maps.n["<leader>Sd"] = nil
+  maps.n["<leader>Sf"] = nil
+  maps.n["<leader>S."] = nil
+end
+
+-- setting a mapping to false will disable it
+-- maps.t["<esc>"] = false
+
+return maps
