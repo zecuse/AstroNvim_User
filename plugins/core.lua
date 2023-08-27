c0e9c6c96a7272d8d6e26606e3abd70d1e15ba98
zecuse <zecuse@ymail.com>
2023-08-27 14:03:22 -0400

    Removed community and updated alpha.

--- plugins/core.lua
+++ plugins/core.lua
@@ -17,6 +17,15 @@ return {
         "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
         "    ██   ████   ████   ██ ██      ██",
       }
+      local button, get_icon = require("astronvim.utils").alpha_button, require("astronvim.utils").get_icon
+      opts.section.buttons.val = {
+        button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+        button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+        button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+        button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+        button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+        button("LDR s l", get_icon("Refresh", 2, true) .. "Last Session  "),
+      }
       return opts
     end,
   },
