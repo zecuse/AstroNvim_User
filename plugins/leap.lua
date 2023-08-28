1ae968b332b6c3efcb2bc2d41814e198827918b6
Cory Williamson <zekos@ymail.com>
2023-08-27 22:11:57 -0400

    Leap and quickscope work!

--- plugins/leap.lua
+++ plugins/leap.lua
@@ -1,3 +1,8 @@
 return {
-  "ggandor/leap.nvim"
+  "ggandor/leap.nvim",
+  lazy = false,
+  dependencies = { "tpope/vim-repeat" },
+  config = function()
+    require("leap").add_default_mappings()
+  end
 }
--- plugins/quickscope.lua
+++ plugins/quickscope.lua
@@ -1,5 +1,6 @@
 return {
   "unblevable/quick-scope",
+  lazy = false,
   init = function()
     -- Trigger a highlight in the appropriate direction when pressing these keys:
     vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
