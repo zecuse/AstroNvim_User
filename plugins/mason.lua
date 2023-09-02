41d8d878bfbc4b3ba78e858d10b22c29ca1e1020
Cory Williamson <zekos@ymail.com>
2023-09-01 23:28:05 -0400

    Added C/C++ debugger support.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -24,6 +24,7 @@ return {
       })
     end,
   },
+  -- use mason-nvim-dap to configure DAP installations
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
@@ -31,6 +32,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "python",
+        "codelldb",
       })
     end,
   },
