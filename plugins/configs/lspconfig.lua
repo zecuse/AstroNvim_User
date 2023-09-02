795078573b9740d9945f14ab7b9217a8b3d29552
Cory Williamson <zekos@ymail.com>
2023-09-01 22:24:29 -0400

    Added C/C++ LSP support to ensure_installs.

---
+++ plugins/configs/lspconfig.lua
@@ -0,0 +1,14 @@
+return function ()
+  local lsp = require "astronvim.utils.lsp"
+  local on_attach = lsp.on_attach
+  local capabilities = lsp.capabilities
+  local lspconfig = require("lspconfig")
+
+  lspconfig.clangd.setup {
+    on_attach = function (client, bufnr)
+      client.server_capabilities.signatureHelpProvider = false
+      on_attach(client, bufnr)
+    end,
+    capabilities = capabilities
+  }
+end
